bb7cdb6514ef7391d12a7456332bc67306afc31c
FulecoRafa <ra.pha@live.com>
2022-12-02 11:27:22 -0300

    feat: Read Txt file testing

--- test/txt.test.ts
+++ test/txt.test.ts
@@ -1,9 +1,15 @@
-import { toTxtFile } from "../src/txt/write";
-import { animals } from "./extra";
+import { fromTxtFile, toTxtFile } from "../src";
+import { Animal, animals } from "./extra";
 import * as fs from "fs";
 
 test('It should write a TXT file', async () => {
     await toTxtFile('animal.txt', animals);
 
     expect(fs.readFileSync('animal.txt', 'utf8')).toBe(fs.readFileSync('test/files/animal.txt', 'utf8'));
+})
+
+test('It should read a TXT file', async () => {
+    const test_animals = await fromTxtFile('test/files/animal.txt', Animal);
+
+    expect(test_animals).toMatchObject(animals);
 })
